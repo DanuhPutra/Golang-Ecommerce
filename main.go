98a03e864adccd83dc5e965f9a8a24fff7f2e643
DanuhPutra <ngurahdanuhputra@gmail.com>
2025-01-28 22:21:59 +0800

    update

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import(
+	"github.com/DanuhPutra/Golang-Ecommerce/controllers"
+	"github.com/DanuhPutra/Golang-Ecommerce/database"
+	"github.com/DanuhPutra/Golang-Ecommerce/middleware"
+	"github.com/DanuhPutra/Golang-Ecommerce/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	
+}
