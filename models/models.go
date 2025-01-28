6d22a0cbf6bfad42371160fe052c0d5617504b46
DanuhPutra <ngurahdanuhputra@gmail.com>
2025-01-28 22:27:37 +0800

    update

--- models/models.go
+++ models/models.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -9,9 +10,9 @@ type User struct {
 	ID primitive.ObjectID `json:"_id" bson:"_id"`
 	First_Name *string `json:"first_name"`
 	Last_Name *string `json:"last_name"`
-	Password *string `json:"password" bson:"password"`
-	Phone *string `json:"phone" bson:"phone"`
-	Token *string `json:"token" bson:"token"`
+	Password *string `json:"password"`
+	Phone *string `json:"phone"`
+	Token *string `json:"token"`
 	Refresh_Token *string `json:"refresh_token"`
 	Created_At *time.Time `json:"created_at"`
 }
